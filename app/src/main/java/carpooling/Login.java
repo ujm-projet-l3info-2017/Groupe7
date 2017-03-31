98b50f1d0140396776899de3d1026d2b53f517e5
Touaibia <adel_231994@hotmail.fr>
2017-03-31 10:15:44 +0200

    [feat]gestion de Register et Login

--- app/src/main/java/carpooling/Login.java
+++ app/src/main/java/carpooling/Login.java
@@ -6,29 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import java.sql.ResultSet;
 
 import com.example.smail.testapp.R;
-
-import java.sql.SQLException;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
     Button bLogin, clickregist;
     EditText etUsername, etPassword;
-    ConnexionDB conexion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        try {
-            conexion = new ConnexionDB();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
@@ -45,45 +35,27 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-       int r =0;
-        ResultSet rs;
 
         switch (v.getId()) {
             case R.id.bclickLogin:
                 String identifiant = etUsername.getText().toString();
                 String mdp = etPassword.getText().toString();
 
-                if (identifiant.trim().equals("")||mdp.trim().equals("")) {
+                if (identifiant.trim().equals(""))
+                {
+                    etUsername.setError("il faut remplir ce champ");
 
-                    r = 1;
                 }
-                if (r==1){
-                    etUsername.setError("il faut remplir tout les champs");
-
+                else if(mdp.trim().equals("")){
+                    etPassword.setError("il faut remplir ce champ");
                 }
                 else
-                    {
-                        try
-                        {
-                            String query = "select * from Utilasateur where username = " + identifiant + " password = " + mdp;
-                            rs = conexion.reqSelect(query);
+                {
 
-                            try {
-                                if (rs.next())
-                                {
-                                    // startActivity(new Intent(this,PageAccueil.class));
-                                }
 
-                                }catch (SQLException e)
-                               {
-                                    e.printStackTrace();
-                                   System.out.print("Compte inexistant");
-                               }
-                        } catch (SQLException e)
-                        {
-                            e.printStackTrace();
-                        }
-                    }
+
+                }
+
                  break;
 
             case R.id.clickRegist:
--- app/src/main/java/carpooling/Register.java
+++ app/src/main/java/carpooling/Register.java
@@ -8,28 +8,22 @@ import android.widget.EditText;
 
 import com.example.smail.testapp.R;
 
-public class Register extends AppCompatActivity implements View.OnClickListener{
+public class Register extends AppCompatActivity implements View.OnClickListener {
     Button bRegister;
-    EditText etName,etUsername,etAge,etPassword,etErreur;
-     ConnexionDB conexion;
+    EditText etName, etUsername, etAge, etPassword, etErreur;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        /*try {
-            conexion = new ConnexionDB();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-*/
-        etName =(EditText)findViewById(R.id.etName);
-        etUsername =(EditText)findViewById(R.id.etUsername);
-        etAge=(EditText)findViewById(R.id.etAge);
-        etPassword=(EditText)findViewById(R.id.etPassword);
-       // etErreur = (EditText)findViewById(R.id.erreur);
-        bRegister = (Button)findViewById(R.id.bRegister);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etAge = (EditText) findViewById(R.id.etAge);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        // etErreur = (EditText)findViewById(R.id.erreur);
+        bRegister = (Button) findViewById(R.id.bRegister);
         bRegister.setOnClickListener(this);
 
     }
@@ -37,50 +31,44 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
     @Override
 
 
-
-    public void onClick(View v)
-    { int r =0;
-        switch (v.getId())
-        {
+    public void onClick(View v) {
+        int r = 0;
+        switch (v.getId()) {
             case R.id.bRegister:
-               int agee = 0;
+                int agee = -1;
                 String nom = etName.getText().toString();
                 String prenom = etUsername.getText().toString();
                 String mdp = etPassword.getText().toString();
-           //   agee =  Integer.parseInt(etAge.getText().toString());
+                agee = Integer.parseInt(etAge.getText().toString());
 
-              if(nom.trim().equals("")||prenom.trim().equals("")||mdp.trim().equals("")){
-
-                  r=1;
-              }
-                if (r == 1)
+                if (nom.trim().equals(""))
                 {
-                  etName.setError("iL faut remplir tout les champs");
-              }
-/*
+                    etName.setError("iL faut remplir tout les champs");
+                }
+                else if (prenom.trim().equals(""))
+                {
+                    etUsername.setError("il faut remplir ce champ ");
+                }
+                else if (mdp.trim().equals(""))
+                {
+                    etPassword.setError("il faut remplir ce champ");
+                }
 
-           else     {
-                      try {
-                             String query = "INSERT into Utilisateur('name, 'username','password','age') VALUES(nom,prenom,mdp,agee)";
-                              conexion.reqSQL(query, 'm');
-                               System.out.print(" reussi");
 
-                         } catch (SQLException e) {
-                          e.printStackTrace();
-                        }
+               else
+                {
+
+
 
                 }
-                */
                 break;
-        }
 
-
+         }
 
     }
 
-
-
-
-
-
 }
+
+
+
+
