d4e9a60bf00ba14ed762a7c453132dc020e4d1ef
Touaibia <adel_231994@hotmail.fr>
2017-04-04 16:52:18 +0200

    Merge branch 'develop' of https://github.com/ujm-projet-l3info-2017/Groupe7
    # Conflicts:
    #	app/src/main/java/carpooling/Login.java

--- app/src/main/java/carpooling/Login.java
+++ app/src/main/java/carpooling/Login.java
@@ -3,6 +3,7 @@ package carpooling;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,15 +40,46 @@ public class Login extends AppCompatActivity implements View.OnClickListener
                 String pseudo = etUsername.getText().toString();
                 String mdp = etPassword.getText().toString();
 
+                Log.d("d_Iden", pseudo);
+                Log.d("d_Mdp", mdp);
+                if (pseudo.trim().equals(""))
+
+
                 if (pseudo.isEmpty())
+
                 {
                     etUsername.setError("This field is required");
                 }
-                if (mdp.isEmpty())
+               else if (mdp.isEmpty()) {
+                        etPassword.setError("This field is required");
+                    }
+                else
                 {
-                    etPassword.setError("This field is required");
+                    String[] response;
+                    ServerCon con = new ServerCon();
+                    con.send(new String[] {con.TYPE_AUTH, pseudo, mdp});
+                    response = con.receive();
+
+                    if (response.length != 2)
+                    {
+                        // TODO: error
+                    }
+                    switch(Integer.parseInt(response[1]))
+                    {
+                        case 0:
+                            // TODO: login impossible
+                            break;
+                        case 1:
+                            // TODO: Login OK
+                            break;
+                        default:
+                            // TODO: error inconnue
+                            break;
+                    }
+
+                    con.closeCon();
                 }
-                break;
+                  break;
 
             case R.id.clickRegist:
                 startActivity(new Intent(this, Register.class));
