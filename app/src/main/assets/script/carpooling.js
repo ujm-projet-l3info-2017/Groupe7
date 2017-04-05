27ff09de5a592e71283ad6a31beead104177c21f
khameds <khameds@gmail.com>
2017-04-05 08:24:57 +0200

    [feat] Add Leflet Routing Machine

--- app/src/main/assets/script/carpooling.js
+++ app/src/main/assets/script/carpooling.js
@@ -1,16 +1,12 @@
-    var map;
-
-    function init(webmap)
-    {
-        map = webmap;
-    }
+var startBtn = createButton("Position de départ");
+var destBtn = createButton('Position d\'arrivée');
 
     function onMapClick(e)
     {
-        popup
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(mymap);
+         var container = L.DomUtil.create('div'),
+                    startBtn = createButton('Position de départ', container),
+                    destBtn = createButton('Position d\'arrivée', container);
+         L.popup().setContent(container).setLatLng(e.latlng).openOn(mymap);
     }
 
     function onLocationError(e)
@@ -18,8 +14,26 @@
         alert(e.message);
     }
 
-    function markLocation(map,lat,long)
+    function markLocation(latitude,longitude, msg)
     {
-
-
+        L.marker([latitude, longitude]).addTo(mymap).bindPopup(msg).openPopup();
     }
+
+    function createButton(label, container) {
+        var btn = L.DomUtil.create('button', '', container);
+
+        btn.setAttribute('type', 'button');
+        btn.innerHTML = label;
+
+        return btn;
+    }
+
+     L.DomEvent.on(startBtn, 'click', function() {
+            control.spliceWaypoints(0, 1, e.latlng);
+            map1.closePopup();
+        });
+
+     L.DomEvent.on(destBtn, 'click', function() {
+             control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+             map1.closePopup();
+         });
