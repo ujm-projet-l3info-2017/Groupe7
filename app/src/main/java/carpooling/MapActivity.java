152436912d01007c19edf428a1e7cf08b975f579
khameds <khameds@gmail.com>
2017-04-13 00:12:56 +0200

    [feat] Properly handle Geolocation permission for Javascript via the webview

--- app/src/main/java/carpooling/MapActivity.java
+++ app/src/main/java/carpooling/MapActivity.java
@@ -2,6 +2,8 @@ package carpooling;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -16,8 +18,19 @@ public class MapActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         WebView myWebView = (WebView) findViewById(R.id.map_view);
+
+        myWebView.setWebChromeClient(new WebChromeClient()
+        {
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback)
+            {
+                callback.invoke(origin, true, false);
+            }
+        });
+
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setGeolocationEnabled(true);
+
         myWebView.loadUrl("file:///android_asset/map.html");
 
     }
